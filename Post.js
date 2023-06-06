0dccc20bab619cd2f1cdb7167fc058d58ba4911d
Dimon <yoh60438@gmail.com>
2023-06-07 00:11:59 +0700

    create basic web-app

---
+++ Post.js
@@ -0,0 +1,14 @@
+class Post {
+  
+    constructor(title) {
+        this.title = title  //set title of post
+        this.date = new Date() // set date of creation
+    }
+
+    toString() {    //return string from instance
+       return JSON.stringify({
+            title: this.title,
+            date: this.date.toJSON()
+        })
+    }
+}
---
+++ analytics.js
@@ -0,0 +1,27 @@
+function createAnalytics() {
+    let counter = 0 //variable to store click count
+    let isDestroyed = false //state flag of Analytics 
+
+    const listener = () => counter++ //incrementor of counter
+
+    document.addEventListener('click', listener) //count number of click on document
+
+    return {
+        //method to destroy Analytcis
+        destroy() {
+            document.removeEventListener('click', listener)
+            isDestroyed = true
+        },
+        
+        //method to get number of clicks
+        getClicks() {
+            //if destroyed
+            if (isDestroyed) return 'Analytics is destroyed'
+
+            return counter
+        }
+
+    }
+}
+
+window.analytics = createAnalytics() //create analytics
